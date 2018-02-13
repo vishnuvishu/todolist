e131eaf11856df8dbe348be1c25d5bd4348c3e34
vishnu vinayan <vishnuv336.com>
2018-02-13 20:22:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoList from './TodoList'
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+var designation = document.querySelector("#container");
+
+ReactDOM.render(
+  <div><TodoList /></div>,
+    designation
+);
+// registerServiceWorker();
